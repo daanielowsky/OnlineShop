b45366392debd6d5ddbdc6227f6feb7cd83f94e2
Daniel Pawlicki <47253118+daanielowsky@users.noreply.github.com>
2019-07-30 11:27:22 +0200

    Created and configurated new repository.

---
+++ src/main/java/com/github/daanielowsky/OnlineShop/Config/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.github.daanielowsky.OnlineShop.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private DataSource dataSource;
+
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("select username, password, true from user where username = ?")
+                .authoritiesByUsernameQuery("select username, 'ROLE_USER' from user where username = ?");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/register").permitAll()
+                .antMatchers("/login").anonymous()
+                .antMatchers("/user", "/user/**").hasRole("USER")
+                .antMatchers("/admin", "/admin/**").hasRole("ADMIN")
+                .antMatchers("/media/**").permitAll()
+                .antMatchers("/offer/*").permitAll()
+                .antMatchers("/search**").permitAll()
+                .antMatchers("/offer/{id}/image").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login?logout")
+                .and()
+                .csrf()
+                .disable()
+                .rememberMe()
+                .key("dupablada")
+                .rememberMeParameter("remember-me")
+                .tokenValiditySeconds(7*24*60*60);
+    }
+}
+
---
+++ src/main/java/com/github/daanielowsky/OnlineShop/Controllers/HomepageController.java
@@ -0,0 +1,15 @@
+package com.github.daanielowsky.OnlineShop.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomepageController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String homepageScreen(){
+        return "HELLO TO MY WEBAPP";
+    }
+}
