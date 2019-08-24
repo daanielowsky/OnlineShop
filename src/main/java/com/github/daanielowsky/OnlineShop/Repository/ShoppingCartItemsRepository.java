65a3be05ff00e054ce3de91746a05cd0eb106320
daanielowsky <pawlicki.daniel@wp.pl>
2019-08-24 15:44:55 +0200

    Shopping Cart improvements

--- src/main/java/com/github/daanielowsky/OnlineShop/Repository/ShoppingCartItemsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.daanielowsky.OnlineShop.Repository;
-
-import com.github.daanielowsky.OnlineShop.Entity.ShoppingCartItems;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShoppingCartItemsRepository extends JpaRepository <ShoppingCartItems, Long> {
-}
